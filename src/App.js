fbbd1901e1ef9a6afe499c0eee0a5c445995a784
ashkanbossani <abossani@hotmail.com>
2022-07-19 14:35:00 -0700

    router set up finsihed making pull request

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
-import './App.scss';
+import "./App.scss";
+import {
+  BroweserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
 const App = () => {
   return (
     <div>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/warehouse" />
+          </Route>
+          <Route
+            path="/warehouse"
+            render={(routeProps) => {
+              // return (
+              // 	<WarehousePage
+              // 		{...routeProps}
+              // 		warehouseList={this.state.warehouseList}
+              // 		inventoryList={this.state.inventoryList}
+              // 	/>
+              // );
+            }}
+          />
+          <Route
+            path="/inventory"
+            render={(routeProps) => {
+              // return (
+              //   <InventoryPage
+              //     {...routeProps}
+              //     warehouseList={this.state.warehouseList}
+              //     inventoryList={this.state.inventoryList}
+              //   />
+              // );
+            }}
+          />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
-}
+};
 
 export default App;

92c4d1c2cca41081178ebe7399ddaefefe4b6739
Valerie Pomerleau <v.pomerleau@gmail.com>
2022-07-18 23:39:07 -0700

    link labels to inputs

--- src/pages/AddNewWarehouse/AddNewWarehouse.js
+++ src/pages/AddNewWarehouse/AddNewWarehouse.js
@@ -57,66 +57,116 @@ class AddNewWarehouse extends Component {
           onSubmit={this.handleSubmit}>
           <fieldset className='warehouse-form__section warehouse-form__section--details'>
             <h3 className='warehouse-form__heading'>Warehouse Details</h3>
-            <label className='warehouse-form__label'>Warehouse Name</label>
-            <input
-              className='warehouse-form__input'
-              name='name'
-              onChange={(e) => this.setState({ name: e.target.value })}
-            />
-            <label className='warehouse-form__label'>Street Address</label>
-            <input
-              className='warehouse-form__input'
-              name='address'
-              onChange={(e) => this.setState({ address: e.target.value })}
-            />
-            <label className='warehouse-form__label'>City</label>
-            <input
-              className='warehouse-form__input'
-              name='city'
-              onChange={(e) => this.setState({ city: e.target.value })}
-            />
-            <label className='warehouse-form__label'>Country</label>
-            <input
-              className='warehouse-form__input'
-              name='country'
-              onChange={(e) => this.setState({ country: e.target.value })}
-            />
+            <label className='warehouse-form__label' htmlFor='name'>
+              Warehouse Name
+              <input
+                className='warehouse-form__input'
+                name='name'
+                value={this.state.name}
+                placeholder='Warehouse Name'
+                // Placeholders would be more useful if they showed example responses instead of repeating input label
+                onChange={(e) => this.setState({ name: e.target.value })}
+              />
+            </label>
+
+            <label className='warehouse-form__label' htmlFor='address'>
+              Street Address
+              <input
+                className='warehouse-form__input'
+                name='address'
+                value={this.state.address}
+                placeholder='Street Address'
+                onChange={(e) => this.setState({ address: e.target.value })}
+              />
+            </label>
+
+            <label className='warehouse-form__label' htmlFor='city'>
+              City
+              <input
+                className='warehouse-form__input'
+                name='city'
+                value={this.state.city}
+                placeholder='City'
+                onChange={(e) => this.setState({ city: e.target.value })}
+              />
+            </label>
+
+            <label className='warehouse-form__label' htmlFor='country'>
+              Country
+              <input
+                className='warehouse-form__input'
+                name='country'
+                value={this.state.country}
+                placeholder='Country'
+                onChange={(e) => this.setState({ country: e.target.value })}
+              />
+            </label>
           </fieldset>
+
           <fieldset className='warehouse-form__section warehouse-form__section--contact'>
             <h3 className='warehouse-form__heading'>Contact Details</h3>
-            <label className='warehouse-form__label'>Contact Name</label>
-            <input
-              className='warehouse-form__input'
-              name='contactName'
-              onChange={(e) => this.setState({ contactName: e.target.value })}
-            />
-            <label className='warehouse-form__label'>Position</label>
-            <input
-              className='warehouse-form__input'
-              name='contactPosition'
-              onChange={(e) =>
-                this.setState({ contactPosition: e.target.value })
-              }
-            />
-            <label className='warehouse-form__label'>Phone Number</label>
-            <input
-              className='warehouse-form__input'
-              name='contactPhone'
-              onChange={(e) => this.setState({ contactPhone: e.target.value })}
-            />
-            <label className='warehouse-form__label'>Email</label>
-            <input
-              className='warehouse-form__input'
-              name='contactEmail'
-              onChange={(e) => this.setState({ contactEmail: e.target.value })}
-            />
+            <label className='warehouse-form__label' htmlFor='contactName'>
+              Contact Name
+              <input
+                className='warehouse-form__input'
+                name='contactName'
+                value={this.state.contactName}
+                placeholder='Contact Name'
+                onChange={(e) => this.setState({ contactName: e.target.value })}
+              />
+            </label>
+
+            <label className='warehouse-form__label' htmlFor='contactPosition'>
+              Position
+              <input
+                className='warehouse-form__input'
+                name='contactPosition'
+                value={this.state.contactPosition}
+                placeholder='Position'
+                onChange={(e) =>
+                  this.setState({ contactPosition: e.target.value })
+                }
+              />
+            </label>
+
+            <label className='warehouse-form__label' htmlFor='contactPhone'>
+              Phone Number
+              <input
+                className='warehouse-form__input'
+                name='contactPhone'
+                value={this.state.contactPhone}
+                placeholder='Phone Number'
+                type='tel'
+                onChange={(e) =>
+                  this.setState({ contactPhone: e.target.value })
+                }
+              />
+            </label>
+
+            <label className='warehouse-form__label' htmlFor='contactEmail'>
+              Email
+              <input
+                className='warehouse-form__input'
+                name='contactEmail'
+                value={this.state.contactEmail}
+                placeholder='Email'
+                type='email'
+                onChange={(e) =>
+                  this.setState({ contactEmail: e.target.value })
+                }
+              />
+            </label>
           </fieldset>
+
           <div className='warehouse-form__button-wrapper'>
             {/* CTA button first in HTML for keyboarding order, reversed visually with flex:row-reverse */}
             <button className='warehouse-form__button warehouse-form__button--CTA'>
               + Add Warehouse
             </button>
-            <button className=' warehouse-form__button warehouse-form__button--cancel'>
+            {/*  TODO check on expected behavior of cancel button: clear form or close form?  */}
+            <button
+              className=' warehouse-form__button warehouse-form__button--cancel'
+              type='reset'>
               Cancel
             </button>
           </div>

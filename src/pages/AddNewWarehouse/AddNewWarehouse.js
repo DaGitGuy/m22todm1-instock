5442de08a88094566006260140f6438f850e1f8c
Valerie Pomerleau <v.pomerleau@gmail.com>
2022-07-18 23:11:07 -0700

    style form for responsiveness

--- src/pages/AddNewWarehouse/AddNewWarehouse.js
+++ src/pages/AddNewWarehouse/AddNewWarehouse.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './AddNewWarehouse.scss';
 import backArrow from '../../assets/icons/arrow_back-24px.svg';
+
+// TODO: Add form input validation
 
 class AddNewWarehouse extends Component {
   state = {
@@ -16,19 +19,43 @@ class AddNewWarehouse extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Submitted!');
+    const warehouseDetails = {
+      name: this.state.name,
+      address: this.state.address,
+      city: this.state.city,
+      country: this.state.country,
+      contactName: this.state.contactName,
+      contactPosition: this.state.contactPosition,
+      contactPhone: this.state.contactPhone,
+      contactEmail: this.state.contactEmail,
+    };
+    console.log('Submitted!', warehouseDetails);
+    axios
+      .post('http://localhost:8080/warehouses/add', warehouseDetails)
+      .then((response) => {
+        console.log(response);
+        e.target.reset();
+        // TODO succcess feedback
+        // TODO page redirect
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   render() {
     return (
-      <div>
-        <div className='page-heading'>
+      <div className='main-container'>
+        <div className='main-heading'>
           <img src={backArrow} />
           <h2>Add New Warehouse</h2>
         </div>
 
-        <form className='warehouse-form' type='submit' onSubmit={this.handleSubmit}>
-          <fieldset className='warehouse-form__section'>
+        <form
+          className='warehouse-form'
+          type='submit'
+          onSubmit={this.handleSubmit}>
+          <fieldset className='warehouse-form__section warehouse-form__section--details'>
             <h3 className='warehouse-form__heading'>Warehouse Details</h3>
             <label className='warehouse-form__label'>Warehouse Name</label>
             <input
@@ -55,7 +82,7 @@ class AddNewWarehouse extends Component {
               onChange={(e) => this.setState({ country: e.target.value })}
             />
           </fieldset>
-          <fieldset className='warehouse-form__section'>
+          <fieldset className='warehouse-form__section warehouse-form__section--contact'>
             <h3 className='warehouse-form__heading'>Contact Details</h3>
             <label className='warehouse-form__label'>Contact Name</label>
             <input
@@ -85,24 +112,16 @@ class AddNewWarehouse extends Component {
             />
           </fieldset>
           <div className='warehouse-form__button-wrapper'>
-            <button className='warehouse-form__button--cancel'>Cancel</button>
-            <button className='warehouse-form__button--CTA'>
+            {/* CTA button first in HTML for keyboarding order, reversed visually with flex:row-reverse */}
+            <button className='warehouse-form__button warehouse-form__button--CTA'>
               + Add Warehouse
+            </button>
+            <button className=' warehouse-form__button warehouse-form__button--cancel'>
+              Cancel
             </button>
           </div>
         </form>
       </div>
-
-      //       "id": "2922c286-16cd-4d43-ab98-c79f698aeab0",
-      // "name": "Manhattan",
-      // "address": "503 Broadway",
-      // "city": "New York",
-      // "country": "USA",
-      // "contact": {
-      //   "name": "Parmin Aujla",
-      //   "position": "Warehouse Manager",
-      //   "phone": "+1 (646) 123-1234",
-      //   "email": "paujla@instock.com"
     );
   }
 }

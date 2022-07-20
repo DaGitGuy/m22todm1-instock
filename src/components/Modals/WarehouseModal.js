ed6f469aa8d94c66c855a72c121ef939f2658dd4
ashkanbossani <abossani@hotmail.com>
2022-07-19 20:29:35 -0700

    Added logic for the buttons function

--- src/components/Modals/WarehouseModal.js
+++ src/components/Modals/WarehouseModal.js
@@ -1,8 +1,37 @@
 import React from "react";
 import "./Modal.scss";
-import DeleteButton from "../../assets/icons/close-24px.svg";
+import XIcon from "../../assets/icons/close-24px.svg";
 
 export default class WarehouseModel extends React.Component {
+  // These can be passed in as props in the future from the warehouse details page
+  // this will be the modal that is displayed when the user clicks on the "Add Warehouse" button
+
+  // showModal = (id) => {
+  //   this.setState({ showModal: true, itemID: id });
+  // };
+
+  // this will be the modal that is displayed when the user clicks on the "cancel button
+
+  // closeModal = () => {
+  //   this.setState({ showModal: false });
+  // };
+
+  // this will delete the inventory assoiciated with the warehouse, This will go in the delete function in the warehouse details ie delete={() => this.handleDelete(this.state.itemID)}/>
+
+  // handleDelete(id) {
+  //   axios
+  //     .delete(`http://localhost:8080/inventory/${id}`)
+  //     .then((res) => {
+  //       this.setState({
+  //         showModal: false,
+  //         inventoryList: res.data,
+  //       });
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }
+
   render() {
     return (
       <>
@@ -13,14 +42,17 @@ export default class WarehouseModel extends React.Component {
               Please confirm that you'd like to delete the Washington from the
               list of warehouses. You won't be able to undo this action.
             </p>
-            <button className="modal-cancel button">Cancel</button>
-            <button className="modal-delete button">Delete</button>
+            <button
+              className="modal-cancel button"
+              onClick={this.props.closeModal}
+            >
+              Cancel
+            </button>
+            <button className="modal-delete button" onClick={this.props.delete}>
+              Delete
+            </button>
             <a href="/" onClick={this.props.closeModal}>
-              <img
-                className="modal__x-icon"
-                src={DeleteButton}
-                alt="x button"
-              />
+              <img className="modal__x-icon" src={XIcon} alt="x button" />
             </a>
           </div>
         </div>

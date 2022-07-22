a7f5fc3073a83854525fbf6a5828b706bd95d8ec
Valerie Pomerleau <v.pomerleau@gmail.com>
2022-07-22 12:29:23 -0700

    update code to handle modal show/hide, and delete action

--- src/components/Modals/DeleteItemModal.js
+++ src/components/Modals/DeleteItemModal.js
@@ -1,82 +1,93 @@
-import React from 'react';
 import './DeleteItemModal.scss';
 import closeIcon from '../../assets/icons/close-24px.svg';
 
 const SERVER_URL =
   process.env.REACT_APP_SERVER_URL || process.env.REACT_APP_SERVER_URL_BACKUP;
 
-class DeleteItemModal extends React.Component {
-  // This will be the starting state which will also be in inventory list component
+const DeleteItemModal = (props) => {
+  /// CODE BELOW TO BE REVIEWED ONCE INVENTORY LIST COMPONENT IS READY
+  // ADD TO INVENTORY LIST and WAREHOUSE DETAILS COMPONENTS CODE
+
+  // Default state
 
   // state = {
   //   showModal: false
   // }
 
-  // These can be passed in as props from the inventory list component
-  // this will be the modal that is displayed when the user clicks on the "Trash Can" button on the inventory list
+  // code to conditionally show/hide delete modal -
 
-  // showModal = () => {
+  // if (this.state.showModal){
+  //   return <DeleteItemModal itemId={this.state.selectedItem} closeModal={this.closeModal}/>
+  //}
+
+  // show modal (called by clicking the trash can icon)
+  // showModal = (id) => {
   //   this.setState(
-  //     {showModal: true}
+  //     {showModal: true, selectedItemId:{id}}
   //   )
   // }
 
-  // this will be the modal that is displayed when the user clicks on the "cancel button
-
-  // closeModal = () => {
+  // hide modal without deleting item (called from modal)
+  // const closeModal = () => {
   //   this.setState({ showModal: false });
   // };
 
-  // this will delete the warehouse, This will go in the delete function in the warehouse details ie delete={() => this.handleDelete(this.handleDelete)}/>
+  // ----- END OF CODE TO BE TRANSFERRED TO INVENTORY LIST AND WAREHOUSE DETAILS ----
 
-  // handleDelete() {
-  //   axios.delete(`${SERVER_URL}/warehouse/${this.props.id}`)
-  //   .then(res => {
-  //     this.setState({
-  //       showModal: false,
-  //     })
-  //    console.log(res.data)
-  //   })
-  //   .catch(err => {
-  //     console.log(err);
-  //   })
-  // }
+  // ADD TO INVENTORY LIST ITEM COMPONENT CODE
 
-  render() {
-    if (!this.props) {
-      return <h1>Loadings...</h1>;
-    }
-    console.log(this.props);
-    return (
-      <div className="container">
-        <div className="modal">
-          {/* Will need to receive itemDetails from props */}
-          {/* <a href="/" onClick={this.props.closeModal}> */}
+  // Add to delete button attributes and pass item id
+  // onClick = this.showModal(id);
+
+  // ----- END OF CODE TO BE TRANSFERRED TO INVENTORY LIST ITEM ----
+
+  // Send delete item request to the API
+  const handleDelete = () => {
+    axios
+      .delete(`${SERVER_URL}/warehouse/${this.props.id}`)
+      .then((res) => {
+        alert('Item deleted!');
+        <Link to="/inventory" />;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handleCancelClick = () =>{
+    this.props.handleCancel();
+  }
+
+  if (!this.props) {
+    return <h1>Loading...</h1>;
+  }
+  return (
+    <div className="container">
+      <div className="modal">
+        {/* Will need to receive itemDetails from props */}
+        <a href="/" onClick={handleCancelClick}>
           <img className="modal__x-icon" src={closeIcon} alt="close" />
-          {/* </a> */}
-          <h1 className="modal__header">Delete an inventory item?</h1>
-          <p className="modal__text">
-            Please confirm that you'd like to delete this item from the
-            inventory list. You won't be able to undo this action.
-          </p>
-          <div className='modal__button-container'>
-            <button
-              className="modal__button modal__button--cancel"
-              // onClick={this.props.closeModal}
-            >
-              Cancel
-            </button>
-            {/* When keyboarding, cancel should be first to prevent accidental selection of destructive action */}
-            <button
-              className="modal__button modal__button--delete"
-              onClick={this.props.delete}>
-              Delete
-            </button>
-          </div>
+        </a>
+        <h1 className="modal__header">Delete {selectedItem} inventory item?</h1>
+        <p className="modal__text">
+          Please confirm that you'd like to delete {selectedItem} from the inventory list. You won't be able to undo this action.
+        </p>
+        <div className="modal__button-container">
+          {/* When keyboarding, cancel should be first to prevent accidental selection of destructive action */}
+          <button
+            className="modal__button modal__button--cancel"
+            onClick={handleCancelClick}>
+            Cancel
+          </button>
+          <button
+            className="modal__button modal__button--delete"
+            onClick={() => {handleDelete(this.props.selectedItemId)}}>
+            Delete
+          </button>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default DeleteItemModal;

3b792733fb2eb3f062f3ffd47d57aae82be14acc
Valerie Pomerleau <v.pomerleau@gmail.com>
2022-07-23 22:41:38 -0700

    load data from API into warehouse details and warehouse inventory list

--- src/components/WarehouseDetails/WarehouseDetails.js
+++ src/components/WarehouseDetails/WarehouseDetails.js
@@ -1,51 +1,69 @@
+import { Link } from "react-router-dom";
 import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
 import editIcon from "../../assets/icons/edit-24px.svg";
 import arrowBack from "../../assets/icons/arrow_back-24px.svg";
 import chevronRight from "../../assets/icons/chevron_right-24px.svg";
 import sortArrow from "../../assets/icons/sort-24px.svg";
 import "./WarehouseDetails.scss";
-import { Link } from "react-router-dom";
 
-function WarehouseDetails({ warehouseData, inventoryData }) {
+function WarehouseDetails(props) {
+  const warehouseId = props.match.params.id;
+
+  // filter warehouses data to keep only the selected warehouses' details
+  const selectedWarehouseArray = Array.from(props.warehouseData).filter(
+    (warehouse) => warehouse.id === warehouseId
+  );
+  const selectedWarehouse = selectedWarehouseArray[0];
+
+  // filter inventory to keep selected warehouse's inventory
+  const filteredInventory = Array.from(props.inventoryData).filter(
+    (item) => item.warehouseID === selectedWarehouse.id
+  );
+
+  if (!props) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
     <main className="details">
       <section className="details__title-box">
         <div className="details__title-row1">
           <img src={arrowBack} />
-          {/* <h2 className='details__title' >{warehouseData.name}</h2> */}
-          <h2 className="details__title">Washington</h2>
+          <h2 className="details__title">{selectedWarehouse.name}</h2>
         </div>
 
         <div className="details__title-row2">
+          <Link to={`/warehouse/${warehouseId}/edit`}>
             <img src={editIcon} />
+          </Link>
         </div>
       </section>
 
       <article className="details__address">
         <section className="details__address-box">
           <p className="details__address-title">WAREHOUSE ADDRESS:</p>
-          {/* <p className='details__address-location' >{`${warehouseData.address}, ${warehouseData.city}, ${warehouseData.country}`}</p> */}
-          <p className="details__address-location">
-            {" "}
-            33 Pearl Street SW, Washington, USA
-          </p>
+          <p className="details__address-location">{`${selectedWarehouse.address}, ${selectedWarehouse.city}, ${selectedWarehouse.country}`}</p>
         </section>
 
         <article className="details__manager">
           <section className="details__manager-box">
             <p className="details__manager-contact">CONTACT NAME:</p>
-            {/* <p className='details__manager-name' >{warehouseData.contact.name}</p> */}
-            <p className="details__manager-name"> Graeme Lyon</p>
-            {/* <p className='details__manager-title' >{warehouseData.contact.position}</p> */}
-            <p className="details__manager-title"> Warehouse Manager</p>
+            <p className="details__manager-name">
+              {selectedWarehouse.contact.name}
+            </p>
+            <p className="details__manager-title">
+              {selectedWarehouse.contact.position}
+            </p>
           </section>
 
           <section className="details__contact">
             <p className="details__contact-info">CONTACT INFORMATION:</p>
-            {/* <p className='details__contact-number' >{warehouseData.contact.phone}</p> */}
-            <p className="details__contact-number"> +1(647) 504-0911</p>
-            {/* <p className='details__contact-email' >{warehouseData.contact.email}</p> */}
-            <p className="details__contact-email">glyon@instock.com</p>
+            <p className="details__contact-number">
+              {selectedWarehouse.contact.phone}
+            </p>
+            <p className="details__contact-email">
+              {selectedWarehouse.contact.email}
+            </p>
           </section>
         </article>
       </article>
@@ -58,11 +76,16 @@ function WarehouseDetails({ warehouseData, inventoryData }) {
             </p>
             <img className="details__sort-arrows" src={sortArrow} />
           </div>
-
-          <div className="details__item-box">
-            <a className="details__inventory-item">Television</a>
-            <img className="details__chevron" src={chevronRight} />
-          </div>
+          {filteredInventory.map((item) => (
+            <div className="details__item-box">
+              <Link
+                to={`/inventory/${item.id}`}
+                className="details__inventory-item">
+                {item.itemName}
+              </Link>
+              <img className="details__chevron" src={chevronRight} />
+            </div>
+          ))}
         </section>
 
         <section className="details__inventory-box">
@@ -72,9 +95,12 @@ function WarehouseDetails({ warehouseData, inventoryData }) {
             </p>
             <img className="details__sort-arrows" src={sortArrow} />
           </div>
-          <div className="details__stock-box">
-            <p className="details__inventory-message">IN STOCK</p>
-          </div>
+
+          {filteredInventory.map((item) => (
+            <div className="details__item-box">
+              <p className="details__inventory-message">{item.status}</p>
+            </div>
+          ))}
         </section>
 
         <section className="details__inventory-box">
@@ -84,7 +110,11 @@ function WarehouseDetails({ warehouseData, inventoryData }) {
             </p>
             <img className="details__sort-arrows" src={sortArrow} />
           </div>
-          <p className="details__category-item">Electronics</p>
+          {filteredInventory.map((item) => (
+            <div className="details__item-box">
+              <p className="details__category-item">{item.category}</p>
+            </div>
+          ))}
         </section>
 
         <section className="details__inventory-box">
@@ -92,14 +122,24 @@ function WarehouseDetails({ warehouseData, inventoryData }) {
             <p className="details__quantity details__header--selected">QTY</p>
             <img className="details__sort-arrows" src={sortArrow} />
           </div>
-          <p className="details__quantity-item">500</p>
+          {filteredInventory.map((item) => (
+            <div className="details__item-box">
+              <p className="details__quantity-item">{item.quantity}</p>
+            </div>
+          ))}
         </section>
+
         <section className="details__inventory-icon">
-          <p className="details__inventory-action">ACTIONS</p>
-          <div className="details__action-icons">
-            <img className="details__delete-icon" src={deleteIcon} />
-            <img className="details__edit-icon" src={editIcon} />
+          <div className="details__header">
+            <p className="details__inventory-action">ACTIONS</p>
           </div>
+
+          {filteredInventory.map((item) => (
+            <div className="details__action-icons">
+              <img className="details__delete-icon" src={deleteIcon} />
+              <img className="details__edit-icon" src={editIcon} />
+            </div>
+          ))}
         </section>
       </article>
     </main>
